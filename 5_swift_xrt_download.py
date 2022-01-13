5b96b6bb4621fe8a800fdd182d82f7ea58758745
Norman Khan <norman1000@gmail.com>
2022-01-13 14:55:21 +0000

    updated

--- 5_swift_xrt_download.py
+++ 5_swift_xrt_download.py
@@ -9,6 +9,40 @@ from swifttools.xrt_prods import XRTProductRequest
 max_jobs = 5
 email = 'nk7g14@soton.ac.uk'
 
+
+sources_names = ['ESO 243-49 HLX-1',
+                 'Ho IX X-1',
+                 'Holmberg II X-1',
+                 'M31 ULX-1',
+                 'J013350.8+303937' # M33 X-8
+                 'RX J133001+47137' # M51 ULX-7
+                 '[LM2005] NGC 3031 ULX1',
+                 'M82 X-2',
+                 'J024025.6-082428' # NGC 1042 ULX1
+                 'NGC1313 X-1',
+                 'NGC1313 X-2',
+                 'J004703.9-204743', # NGC 247 ULX1
+                 '[LB2005] NGC 253 X20', # NGC 253 ULX1
+                 'NGC 300 ULX1',
+                 '[LM2005] NGC 4395 ULX1',
+                 '[LM2005] NGC 5204 ULX1',
+                 '[LM2005] NGC 5408 ULX1',
+                 '[SRW2006b] NGC 55 ULX',
+                 'J141939.3+564137', # NGC 5585 ULX (overlapping sources)
+                 'NAME NGC 5907 ULX',
+                 '[LB2005] NGC 6946 ULX1',
+                 'J213631.9-543357', # NGC 7090
+                 'NGC7793 P13',
+                 'NAME NGC 925 ULX-1',
+                 'NAME NGC 925 ULX-2',
+                 'SMC X-3',
+                 'SS433',
+                 'SWIFT J0243.6+6124',
+                 'NAME UGC 6456 ULX',
+
+
+
+                 '
 requests = []
 
 
@@ -18,44 +52,39 @@ for s in source_dirs:
     print(s, source_name)
 
 req = XRTProductRequest(email)
-global_pars = {'name': 'NGC 300 ULX-1',
-               'targ': '00095672',
-               'T0': None,
-               'SinceT0': None,
-               'RA': None,
-               'Dec': None,
+
+global_pars = {'name': 'sourcename',
+               #'targ': None,
+               #'T0': None,
+               #'SinceT0': None,
+               'RA': 359.462083,
+               'Dec': -32.624056,
                'centroid': True,
                'centMeth': 'simple',
-               'maxCentTries': None,
+               #'maxCentTries': None,
                'posErr': 1,
-               'sss': None,
+               #'sss': None,
                'useSXPS': False,
-               'wtPupRate': None,
-               'pcPupRate': None,
+               #'wtPupRate': None,
+               #'pcPupRate': None,
                'notify': True,
-               'getCoords': True,
-               'getTargs': None,
-               'getT0': None}
+               'getCoords': False,
+               'getTargs': False}
+               #'getT0': None}
 
 
 
 
-req.setGlobalPars(name='NGC 300 ULX-1',
-                  targ='00095672',
-                  getCoords=True,
-                  centroid=True,
-                  centMeth='simple',
-                  useSXPS=False,
-                  posErr=1)
+req.setGlobalPars(**global_pars)
+
 req.addLightCurve(binMeth='obsid', timeType='m')
 req.addSpectrum(hasRedshift=False)
 
 assert req.isValid()[0]
 #req.submit()
-#req.submitError
+print(req.submitError)
 
 while not req.complete:
-    print('a')
     time.sleep(10)
 
 print(req.getAllPars())
---
+++ download_scripts/SS433.sh
@@ -0,0 +1,52 @@
+# The following commands will download selected data products.
+#
+# Note: The network utility wget is included with most
+# systems.  To download wget or get more information visit the
+# GNU website http://www.gnu.org/software/wget/wget.html
+#
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_09//00046323001/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_11//00080803005/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_11//00046323003/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2015_04//00080803010/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2006_07//00035190015/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_07//00035190027/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_10//00080803001/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_10//00035190034/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_11//00080803004/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2018_07//00035190036/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2021_07//00089239001/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2005_08//00035190007/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2005_08//00035190013/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2007_09//00035190025/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_10//00035190030/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2007_09//00035190019/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2012_10//00035190026/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2007_09//00035190022/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2007_09//00035190021/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_10//00035190033/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2005_07//00035190001/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2005_07//00035190002/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2015_06//00035190035/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_10//00035190032/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2015_03//00080803009/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_09//00035190028/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2007_09//00035190023/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2015_03//00080803008/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2005_08//00035190005/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2006_11//00035190016/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2007_09//00035190018/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2007_09//00035190020/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_11//00046323002/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2006_06//00035190014/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_10//00035190031/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2015_02//00080803006/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2005_08//00035190006/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2019_12//00088820001/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2015_07//00080803011/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_10//00080803002/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2005_08//00035190008/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2018_08//00035190037/
+wget -q -nH --no-check-certificate --cut-dirs=5 -r -l0 -c -N -np -R 'index*' -erobots=off --retr-symlinks https://heasarc.gsfc.nasa.gov/FTP/swift/data/obs/2014_10//00080803003/
+
+# Total size of data product files local to the HEASARC system: 2 GB
+# File sizes of remote data products are not available
