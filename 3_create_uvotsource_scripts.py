131a20191f3c5d18181e88f18ba55fe5e1299a02
Norman Khan <norman1000@gmail.com>
2022-02-23 10:51:54 +0000

    updated scripts

---
+++ 3_create_uvotsource_scripts.py
@@ -0,0 +1,39 @@
+from glob import glob
+import pandas as pd
+
+source_dirs = glob('download_scripts/*/')
+
+all_res = []
+
+all_uvotmaghist_paths = []
+
+for s in source_dirs:
+    uvotsource_sh_path = s + 'uvotsource_all.sh'
+    all_uvotmaghist_paths.append(uvotsource_sh_path)
+    img_files = glob(f'{s}*/*_sk.img.gz')
+    src_reg_files = glob(f'{s}*src*.reg')
+
+    maghist_lines = []
+    for infile in img_files:
+        infile = '/'.join(infile.split('/')[2:])
+
+        for src in src_reg_files:
+            srcreg = src.split('/')[-1]
+            outfile = infile[:-7] + '_' + srcreg[:-4] + '_uvotsource.fits'
+            maghist_line = f'uvotsource image={infile} outfile={outfile} srcreg={srcreg} bkgreg=bkg.reg sigma=3.0\n'
+            maghist_lines.append(maghist_line)
+
+    print(f'Creating {uvotsource_sh_path:<60} lines={len(maghist_lines)}')
+    with open(uvotsource_sh_path, 'w+') as f:
+        for l in maghist_lines:
+            f.write(f'{l}')
+
+
+sh_path = 'uvotsource_all_sources.sh'
+print(f'Creating {sh_path}')
+
+with open(sh_path, 'w+') as f:
+    for l in source_dirs:
+        f.write(f'cd {l}\n')
+        f.write(f'./uvotsource_all.sh\n')
+        f.write('cd ../..\n')
---
+++ 4_combine_all_uvotsource.py
@@ -0,0 +1,45 @@
+import os
+from glob import glob
+import re
+import warnings
+
+from astropy.units import UnitsWarning
+from astropy.table import Table, vstack
+from tqdm import tqdm
+
+warnings.filterwarnings('ignore', category=UnitsWarning, append=True)
+
+
+source_dirs = glob('download_scripts/*/')
+
+for s in source_dirs[1:]:
+    print(f'Doing {s}')
+    src_reg_files = glob(f'{s}*src*.reg')
+
+    for src in src_reg_files:
+        srcreg = src.split('/')[-1][:-4]
+        uvotsource_files = glob(f'{s}*/*{srcreg}_uvotsource*')
+        uvotsource_fits = s+f'{srcreg}_uvotsource_all.fits'
+        
+        if len(uvotsource_files) == 0:
+            print('No uvotsource files found')
+            continue
+        
+        if os.path.exists(uvotsource_fits):
+            print(f'{uvotsource_fits} already exists, skipping...')
+            continue
+
+
+ 
+        all_tabs = []
+
+        for f in tqdm(uvotsource_files):
+            tab = Table.read(f)
+            tab['OBSID'] = re.search(r'\d{11}', f)[0]
+            all_tabs.append(tab)
+
+        table = vstack(all_tabs)
+
+
+        print(f'Saving table to {uvotsource_fits}')
+        table.write(uvotsource_fits, format='fits')
