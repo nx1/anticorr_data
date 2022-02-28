d36662f17354032233c869c8317fb0b1b3e5bc83
Norman Khan <norman1000@gmail.com>
2022-02-28 11:35:38 +0000

    updated py

---
+++ query_nustar.py
@@ -0,0 +1,39 @@
+from source_names import source_names
+import numpy as np
+import pandas as pd
+from astroquery.heasarc import Heasarc
+from astropy.table import vstack
+
+h = Heasarc()
+
+all_summary = []
+all_tabs = []
+
+for i, s in enumerate(source_names):
+    summary = {}
+    print(f'Querying {s} ({i}/{len(source_names)})')
+    s_sp = s.replace('_',' ') # Heasarc does not like underscores :(
+    try:
+        tab = h.query_object(s_sp, mission='NUMASTER', resultmax=3000)
+    except:
+        pass
+    tab['SEARCH_NAME'] = s
+    print(tab)
+    summary['source_name'] = s
+    summary['n_obs'] = len(tab)
+    print(summary)
+    all_summary.append(summary)
+    all_tabs.append(tab)
+    
+df_all_summary = pd.DataFrame(all_summary).sort_values('n_obs', ascending=False)
+tab_master = vstack(all_tabs)
+print(df_all_summary)
+
+savepath = 'tables/numaster_n_obs.csv'
+savepath_master = 'tables/nustarmaster.csv'
+print(f'saving to {savepath}')
+print(f'saving to {savepath_master}')
+df_all_summary.to_csv(savepath, index=False)
+tab_master.write(savepath_master)
+n_obs_tot = df_all_summary["n_obs"].sum()
+print(f'Total obs={n_obs_tot} \t estimated size = {0.025*n_obs_tot} gb')
